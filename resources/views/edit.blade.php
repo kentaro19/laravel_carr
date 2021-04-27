a78c6632624f69c165f1f1cd271370f5aa02dcad
kentaro19 <kentaro.ushirooka@gmail.com>
2021-04-27 16:50:02 +0900

    edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -9,22 +9,22 @@
     </head>
     
     <body>
-        <h1>Blog Name</h1>
-        
-            <form action="/posts" method="POST">
-              @csrf
-              <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
-              </div>
-              <div class="body">
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
-              </div>
-              <input type="submit" value="保存"/>
-            </form>
-            <div class="back">
-                [ <a href="/">back</a>]
+        <h1 class="title">編集画面</h1>
+    <div class="content">
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class='content__title'>
+                <h2>タイトル</h2>
+                <input type='text' name='post[title]' value="{{ $post->title }}">
             </div>
+            <div class='content__body'>
+                <h2>本文</h2>
+                <textarea type='text' name='post[body]'>{{ $post->body }}</textarea>
+            </div>
+            <input type="submit" value="update">
+        </form>
+        <div class="back">[<a href="/posts/{{ $post->id }}">back</a>]</div>
+    </div>
     </body>
 </html>
